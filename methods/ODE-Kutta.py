57e63e56f3420d538a08ba3b28a3e0ce6ae412c0
Abdullah Adel <abdullahadel.aam@gmail.com>
2020-05-24 12:46:45 +0200

    Added ODE-Kutta.py

--- methods/ODE-Kutta.py
+++ methods/ODE-Kutta.py
@@ -1,36 +1,47 @@
 # Python program to implement Runge Kutta method 
 # function to get the value of f(x,y)
-def functionOf(x, y): 
-    return eval(equation)  
-# Finds value of y for a given x using step size h 
+import sympy
+
+def functionOf(x, y, equ):
+    X = sympy.symbols('x')
+    Y = sympy.symbols('y')
+
+    return sympy.sympify(equ).evalf(subs={X: x, Y: y})
+
+
+# Finds value of y for a given x using step size h
 # and initial value y0 at x0. 
-def rungeKutta(x0, y0, x, h): 
-    # Count number of iterations using step size or 
+def rungeKutta(x0, y0, x, h, equ):
+    # Count number of iterations using step size or
     # step height h 
-    n = (int)((x - x0)/h)  
+    n = int((x - x0) / h)
+    iterations = []
     # Iterate for number of iterations 
-    y = y0 
-    for i in range(1, n + 1): 
+    y = y0
+    for i in range(1, n + 1):
         "Apply Runge Kutta Formulas to find next value of y"
-        k1 = h * functionOf(x0, y) 
-        k2 = h * functionOf(x0 + 0.5 * h, y + 0.5 * k1) 
-        k3 = h * functionOf(x0 + 0.5 * h, y + 0.5 * k2) 
-        k4 = h * functionOf(x0 + h, y + k3) 
-  
+        k1 = h * functionOf(x0, y, equ)
+        k2 = h * functionOf(x0 + 0.5 * h, y + 0.5 * k1, equ)
+        k3 = h * functionOf(x0 + 0.5 * h, y + 0.5 * k2, equ)
+        k4 = h * functionOf(x0 + h, y + k3, equ)
+
         # Update next value of y 
-        y = y + (1.0 / 6.0)*(k1 + 2 * k2 + 2 * k3 + k4) 
-  
+        y = y + (1.0 / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4)
+        iterations.append(y)
+
         # Update next value of x 
-        x0 = x0 + h 
-    return y 
-  
-# Driver method 
+        x0 = x0 + h
+    return iterations
+
+
+# Driver method
 def Demo():
-    global equation
-    equation=input("Enter The Equation ")
+    equation = input("Enter The Equation ")
     x0 = 0
-    y = 1
-    x = 2
-    h = 0.2
-    print ('The value of y at x is:', rungeKutta(x0, y, x, h)) 
+    y0 = 1
+    x = 0.1
+    h = 0.001
+    print('The value of y at x is:', rungeKutta(x0, y0, x, h, equation)[-1])
+
+
 Demo()
