6a5580bcf22c61f877b9d93a9c075c5f7b45bea1
hoskillua <47090776+hoskillua@users.noreply.github.com>
2020-05-26 03:17:11 +0200

    Scatter-Done :D
    Fixed some issues with points plotting

--- static/js/LeastSquareReg.js
+++ static/js/LeastSquareReg.js
@@ -1,17 +1,17 @@
 var parameters = {
     target: '#myFunction', data: [
         {
+            fn: '-1000000',
+            color: "#000000",
+            graphType: 'polyline'
+        },
+        {
             points: [
             ],
             fnType: 'points',
             graphType: 'scatter',
             color: "#000000"
 
-        },
-        {
-            fn: '-1000000',
-            color: "#000000",
-            graphType: 'polyline'
         }
     ],
     grid: false,
@@ -19,6 +19,19 @@ var parameters = {
     xAxis: { domain: [-5, 5] },
     height: window.innerWidth / 100 * 18.45,
     width: window.innerWidth / 100 * 19.13
+};
+
+function Scatter(el) {
+    var elx = el.parentElement.parentElement.childNodes[1].childNodes[0];
+    var ely = el.parentElement.parentElement.childNodes[3].childNodes[0];
+
+
+    if (elx.value && ely.value) {
+        var point = [parseFloat(elx.value), parseFloat(ely.value)];
+        parameters.data[1].points.push(point);
+    }
+
+    functionPlot(parameters);
 };
 
 
@@ -30,13 +43,17 @@ function addtograph() {
         var func = document.getElementsByClassName("Rectangle_Essam")[0].value;
         for (let i = 0; i < 9; i++)
             func = func.replace('**', '^');
-        parameters.data[1].fn = func;
+        parameters.data[0].fn = func;
     }
     functionPlot(parameters);
 }
 
 window.onload = function () {
     addtograph();
+    for(var i = 0; i <= 14; i++){
+        Scatter(document.getElementsByName("x_coordinates" + i)[0]);
+        Scatter(document.getElementsByName("y_coordinates" + i)[0]);
+    }
 };
 
 function substitute(el) {
@@ -68,17 +85,5 @@ function DimInput() {
         document.getElementById('R__x____').setAttribute("style", "");
         document.getElementsByClassName('Rectangle_32')[0].setAttribute("style", "text-align: left;");
     }
-}
-function scatter(el) {
-    var elx = el.parentelement.parentelement.childnodes[1].childnodes[0];
-    var ely = el.parentelement.parentelement.childnodes[3].childnodes[0];
-
-
-    if (elx.value && ely.value) {
-        var point = [parsefloat(elx.value), parsefloat(ely.value)];
-        parameters.data[0].points.push(point);
-    }
-
-    functionplot(parameters);
 }
 
