fb5498f9023fa2f7a215deea019dc25d8d278f52
YousefGamal220 <57014132+YousefGamal220@users.noreply.github.com>
2020-06-11 00:37:04 +0200

    Regular PDE

--- app.py
+++ app.py
@@ -1224,20 +1224,20 @@ def background_process():
 
         try:
             for i in range(4) :
-                f_str = 'function' + str(i)
+                #f_str = 'function' + str(i)
                 x_i_str = 'xi' + str(i)
                 x_f_str = 'xf' + str(i)
                 y_i_str = 'yi' + str(i)
                 y_f_str ='yf' + str(i)
                 u_str ='u' + str(i)
                 #Parameters
-                f = request.form[f_str]
+                #f = request.form[f_str]
                 x_i = request.form[x_i_str]
                 x_f = request.form[x_f_str]
                 y_i = request.form[y_i_str]
                 y_f = request.form[y_f_str]
                 u = request.form[u_str]
-                if not f=="" and not x_i=="" and not x_f=="" and not y_i=="" and not y_f=="" and not u=="":
+                if not x_i=="" and not x_f=="" and not y_i=="" and not y_f=="" and not u=="":
                     x_i = float(request.form[x_i_str])
                     x_f = float(request.form[x_f_str])
                     y_i = float(request.form[y_i_str])
@@ -1246,12 +1246,12 @@ def background_process():
                     xf_list.append(x_f)
                     yi_list.append(y_i)
                     yf_list.append(y_f)
-                    function_list.append(f)
+        
                     value_list.append(u)
                     boundry_counter = boundry_counter + 1
 
                 #Open Boundary Condition
-                elif f=="" and x_i=="" and x_f=="" and not y_i=="" and not y_f=="" and not u=="":
+                elif x_i=="" and x_f=="" and not y_i=="" and not y_f=="" and not u=="":
                     yy = int(y_i)
                     Rows = int(y_f)
                     Value = int(u)
@@ -1270,10 +1270,10 @@ def background_process():
         if boundry_counter==4:
             try:
                 # L R U D
-                #print("You Are in this Region")
+               
                 UList = Closed_Region(value_list[1], value_list[2], value_list[0], value_list[3], h, k,
                                       x1, x2, y1, y2, dxx, dyy, dx, dy, u_coeff, dxy, function)
-                #print(UList)
+                
             except:
                 return jsonify(error = 'Could not Solve')
             try:
@@ -1285,11 +1285,10 @@ def background_process():
 
             x_index = (x_point-x1)/h
             y_index = (y_point-y1)/k
-            #print(x_index)
-            #print(y_index)
+
             try:
                 U = UList[int(x_index)][int(y_index)]
-                #print(U)
+
             except:
                 return jsonify(error = 'Invalid Point', U = '')
 
@@ -1308,7 +1307,9 @@ def background_process():
             Number_Of_Rows = y_index + 1
 
             try:
-
+                # yy --> y_i
+                # Rows -->y_f
+                # value --> function
                 UList = Open_Region(value_list[0], value_list[1], value_list[2], h, k, x1, x2,
                                     y1, dxx, dyy, dx, dy, u_coeff, dxy, function, Value, yy, int(Number_Of_Rows))
 
